c71eaa81db7e4f2478e43336c69c0a8e2bfcaa02
feng <liyufeng1515@gmail.com>
2016-08-11 16:13:04 +0800

    fixed bug

--- importProduct/importProductsByWave-2.py
+++ importProduct/importProductsByWave-2.py
@@ -16,7 +16,7 @@ htmlPage = opener.open(config.serviceUrl(config.olapIP80),data).read()
 soup = BeautifulSoup(htmlPage,"html.parser",from_encoding="utf-8")
 div = soup.find("div",class_="screenlet-body")
 if "success" in div.prettify():
-	print "Result: Success."
+    print "Result: Success."+div.get_text()[div.get_text().index("successMessage"):div.get_text().index("Clear")]
 else:
 	for i in div.form.findAll("td"):
 		if i.string == None or i.string == 'Save Value ?' or i.string == "Parameters Value" or i.string == "Parameter Name":
